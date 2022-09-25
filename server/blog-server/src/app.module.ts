442b2d86b17eefeecfa77469b3b175e60a3f2c73
Alexander <kirumasuta@gmail.com>
2022-09-25 16:38:36 +1000

    stable back for dev

--- server/blog-server/src/app.module.ts
+++ server/blog-server/src/app.module.ts
@@ -1,9 +1,9 @@
 import {ArticleModule} from "./article/article.module";
-import {SequelizeModule} from "@nestjs/sequelize";
 import {ConfigModule} from "@nestjs/config";
 import {Sequelize_configService} from "./config/sequelize_config.service";
 import {databaseConfig} from "./config/configuration";
 import {Module} from "@nestjs/common";
+import {SequelizeModule} from "@nestjs/sequelize";
 
 @Module({
   imports: [SequelizeModule.forRootAsync({
